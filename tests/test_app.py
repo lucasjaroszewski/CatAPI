584f691cd113ec710c9869d1a8a92749cf871c0a
Lucas Jaroszewski <ljnunes@outlook.com.br>
2021-05-18 12:06:51 -0300

    Updated DELETE pytest.

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,4 @@
 from starlette.testclient import TestClient
-import json
 from app.main import app
 
 client = TestClient(app)
@@ -49,6 +48,7 @@ def test_add_cat_invalid_body_size():
     assert response.json() == {'detail': 'Body type must be Small, Medium or Large'}
 
 
+# Validation of PATCH request
 def test_update_cat():
     response = client.patch("/cats/Ragdoll", json={'breed': 'Ragdoll',
                                                    'location_of_origin': 'Russia',
@@ -63,6 +63,18 @@ def test_update_cat():
                                'pattern': 'Black And White'}
 
 
+# Validation if cat exists
+def test_update_cat_not_found():
+    response = client.patch("/cats/Unknown", json={'breed': 'Unknown',
+                                                   'location_of_origin': 'United States',
+                                                   'coat_length': 5,
+                                                   'body_type': 'Small',
+                                                   'pattern': 'Black And White'})
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'Cat not found.'}
+
+
+# Validation of positive integer
 def test_update_cat_invalid_coat_length():
     response = client.patch("/cats/Ragdoll", json={'breed': 'Ragdoll',
                                                    'location_of_origin': 'Russia',
@@ -73,6 +85,7 @@ def test_update_cat_invalid_coat_length():
     assert response.json() == {'detail': 'Coat length must be equal or greater than 0.'}
 
 
+# Validation if body size is inside given list
 def test_update_cat_invalid_body_type():
     response = client.patch("/cats/Ragdoll", json={'breed': 'Ragdoll',
                                                    'location_of_origin': 'Russia',
@@ -81,3 +94,10 @@ def test_update_cat_invalid_body_type():
                                                    'pattern': 'Black And White'})
     assert response.status_code == 406
     assert response.json() == {'detail': 'Body type must be Small, Medium or Large'}
+
+
+# Validation of DELETE request
+def test_remove_cat():
+    response = client.delete("/cats/Ragdoll")
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'Cat not found.'}
